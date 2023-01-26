4227ba4cfae3f9bce620fc6424ae5230f4b799af
Your Name <you@example.com>
2023-01-26 17:58:45 +0800

    update cf

--- cf/contest/1782/D.cpp
+++ cf/contest/1782/D.cpp
@@ -1,11 +1,39 @@
 #include <bits/stdc++.h>
 // #define SINGLE_INPUT
 #define ll long long
-#define N 500005
+#define N 55
 #define MOD 998244353
 using namespace std;
 
-void sol() {}
+ll a[N];
+
+void sol() {
+    map<ll, set<ll>> mp;
+    int n;
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            ll v = a[j] - a[i];
+            for (ll x = 1; x * x <= v; x++) {
+                if (v % x)
+                    continue;
+                ll y = v / x;
+                if ((x + y) % 2 == 0 && a[j] <= (x + y) / 2 * (x + y) / 2) {
+                    mp[(x + y) / 2 * (x + y) / 2 - a[j]].insert(a[i]);
+                    mp[(x + y) / 2 * (x + y) / 2 - a[j]].insert(a[j]);
+                }
+            }
+        }
+    }
+    int ans = 1;
+    for (auto [i, j] : mp) {
+        ans = max(ans, (int)j.size());
+    }
+    cout << ans << "\n";
+}
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
