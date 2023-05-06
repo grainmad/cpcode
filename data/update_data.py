101fd0ccf00186e6a5f1751b3b086766655fc61b
grainmad <2458898755@qq.com>
2023-05-06 22:58:05 +0800

    add update data script

---
+++ data/update_data.py
@@ -0,0 +1,14 @@
+import requests
+import json
+
+rsp = json.loads(requests.get(
+    url="https://codeforces.com/api/problemset.problems").text)
+# print(rsp)
+if rsp['status'] == "OK":
+    print("update data success")
+    data = {"problemStatistics": rsp["result"]
+            ["problemStatistics"], "problems": rsp["result"]["problems"]}
+    with open("data/all.json", "w", encoding="utf8") as f:
+        json.dump(data, f)
+else:
+    print(rsp)
