69930b2339efea6d81edeeafeb510d6f7cde9975
grainmad <2458898755@qq.com>
2023-05-09 00:06:23 +0800

    update

---
+++ cf/a-problem-everyday/2023/05/07/424C.cpp
@@ -0,0 +1,58 @@
+
+#include <bits/stdc++.h>
+#define SINGLE_INPUT
+#define ll long long
+#define N 500005
+#define MOD 998244353
+using namespace std;
+
+void sol() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    int ans = 0;
+    for (int& i : a)
+        cin >> i, ans ^= i;
+    vector<int> d(n + 5);
+    for (int i = 1; i <= n; i++) {
+        int o = n / i % 2;
+        if (n % i == 0) {
+            if (o) {  // [0, i-1]
+                d[0]++;
+                d[i]--;
+            }
+        } else {
+            if (o) {  // [0] and [n%i+1, i-1]
+                d[0]++;
+                d[1]--;
+                d[n % i + 1]++;
+                d[i]--;
+            } else {  // [1, n%i]
+                d[1]++;
+                d[n % i + 1]--;
+            }
+        }
+    }
+    int c = 0;
+    for (int i = 0; i <= n; i++) {
+        c += d[i];
+        if (c % 2)
+            ans ^= i;
+    }
+    cout << ans << "\n";
+}
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef SINGLE_INPUT
+    int t;
+    cin >> t;
+    while (t--) {
+        sol();
+    }
+#else
+    sol();
+#endif
+    return 0;
+}
--- cf/a-problem-everyday/gen-problem.py
+++ cf/a-problem-everyday/gen-problem.py
@@ -112,7 +112,7 @@ def get_existed_promblems(dir):
 def random_problem(all_problems):
     condition = {
         "rating-lest": 1600,
-        "rating-most": 2100,
+        "rating-most": 2000,
         # [] 空则选取所有标签
         "include-tags": ["constructive algorithms", "greedy", "math"]
 
