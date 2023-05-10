94c2390b531e1460069ed7e320ddd90e179ce7ba
grainmad <2458898755@qq.com>
2023-05-10 22:13:20 +0800

    ape 5.10

--- cf/a-problem-everyday/2023/05/10/746C.cpp
+++ cf/a-problem-everyday/2023/05/10/746C.cpp
@@ -1,14 +1,68 @@
 
 #include <bits/stdc++.h>
-// #define SINGLE_INPUT
+#define SINGLE_INPUT
 #define ll long long
 #define N 500005
 #define MOD 998244353
 using namespace std;
 
-
 void sol() {
+    int s, x1, x2, t1, t2, p, d;
+    cin >> s >> x1 >> x2 >> t1 >> t2 >> p >> d;
+    int a = abs(x2 - x1) * t2;
+    if (t1 > t2) {
+        cout << a << "\n";
+        return;
+    }
+    if (x1 < x2) {
+        // if (p <= x1) {
+        //     if (d == 1) {
+        //         a = min(a, (x2 - p) * t1);
+        //     } else {
+        //         a = min(a, (p + x2) * t1);
+        //     }
+        // } else {
+        //     if (d == 1) {
+        //         a = min(a, (s-p+s+x2)*t1);
+        //     } else {
+        //         a = min(a, (p+x2)*t1);
+        //     }
+        // }
+        if (d == 1) {
+            if (p <= x1) {
+                a = min(a, (x2 - p) * t1);
+            } else {
+                a = min(a, (s - p + s + x2) * t1);
+            }
+        } else {
+            a = min(a, (p + x2) * t1);
+        }
 
+    } else {
+        // if (p >= x1) {
+        //     if (d == -1) {
+        //         a = min(a, (p - x2) * t1);
+        //     } else {
+        //         a = min(a, (s-p + s-x2) * t1);
+        //     }
+        // } else {
+        //     if (d == -1) {
+        //         a = min(a, (p+s+s-x2)*t1);
+        //     } else {
+        //         a = min(a, (s-p+s-x2)*t1);
+        //     }
+        // }
+        if (d == -1) {
+            if (p >= x1) {
+                a = min(a, (p - x2) * t1);
+            } else {
+                a = min(a, (p + s + s - x2) * t1);
+            }
+        } else {
+            a = min(a, (s - p + s - x2) * t1);
+        }
+    }
+    cout << a << "\n";
 }
 int main() {
     ios::sync_with_stdio(false);
--- cf/a-problem-everyday/2023/05/10/900C.cpp
+++ cf/a-problem-everyday/2023/05/10/900C.cpp
@@ -1,14 +1,64 @@
 
 #include <bits/stdc++.h>
-// #define SINGLE_INPUT
+#define SINGLE_INPUT
 #define ll long long
-#define N 500005
+#define N 100005
 #define MOD 998244353
 using namespace std;
 
+int A[N];
+
+void add(int x) {
+    for (int i = x; i < N; i += i & -i) {
+        A[i]++;
+    }
+}
+
+int ask(int x) {
+    int rt = 0;
+    for (int i = x; i > 0; i = i & (i - 1)) {
+        rt += A[i];
+    }
+    return rt;
+}
 
 void sol() {
-
+    int n;
+    cin >> n;
+    vector<int> st, c(n + 1, 0), rec(n + 1, 0);
+    int sz = 0;
+    // rec[i] i是否已经是 record
+    // c[i] 删除i后能能产生的record数
+    // 对于i，删掉它，record 将变为 record总数-rec[i]+c[i]
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        add(x);
+        while (st.size() && st.back() < x)
+            st.pop_back();
+        if (st.size() == 1 && ask(x) == i)
+            c[st.back()]++;
+        st.push_back(x);
+        if (st.size() == 1) {
+            sz++;
+            rec[x]++;
+        }
+    }
+    // for (int i = 1; i <= n; i++) {
+    //     if (rec[i])
+    //         cout << i << " ";
+    // }
+    // cout << "\n";
+    int mx = 0, ans = 1;
+    for (int i = n; i > 0; i--) {
+        int e = sz - rec[i] + c[i];
+        // cout << i << " " << e << "\n";
+        if (e >= mx) {
+            mx = e;
+            ans = i;
+        }
+    }
+    cout << ans << "\n";
 }
 int main() {
     ios::sync_with_stdio(false);
