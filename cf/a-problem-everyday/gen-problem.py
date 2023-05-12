943cdd2a6596f93956214cd71108d2515c98ad20
grainmad <2458898755@qq.com>
2023-05-12 21:59:30 +0800

    temp

--- cf/a-problem-everyday/gen-problem.py
+++ cf/a-problem-everyday/gen-problem.py
@@ -111,7 +111,7 @@ def get_existed_promblems(dir):
 
 def random_problem(all_problems):
     condition = {
-        "rating-lest": 1600,
+        "rating-lest": 1700,
         "rating-most": 2000,
         # [] 空则选取所有标签
         "include-tags": ["constructive algorithms", "greedy", "math"]
