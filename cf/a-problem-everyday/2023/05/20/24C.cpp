0f9882b6404a4982e38f2f0627c19e5dc8cec780
grainmad <2458898755@qq.com>
2023-05-23 00:24:19 +0800

    tmp upd 5.23

--- cf/a-problem-everyday/2023/05/20/24C.cpp
+++ cf/a-problem-everyday/2023/05/20/24C.cpp
@@ -1,15 +1,26 @@
 
 #include <bits/stdc++.h>
-// #define SINGLE_INPUT
+#define SINGLE_INPUT
 #define ll long long
 #define ull unsigned long long
-#define N 500005
+#define N 200005
 #define MOD 998244353
 using namespace std;
 
+int mx[N], my[N], ax[N], ay[N];
 
 void sol() {
-
+    ll n, j;
+    cin >> n >> j;
+    cin >> mx[0] >> my[0];
+    for (int i = 0; i < n; i++) {
+        cin >> ax[i] >> ay[i];
+    }
+    for (int i = 1; i < 2 * n; i++) {
+        mx[i] = 2 * ax[(i - 1) % n] - mx[i - 1];
+        my[i] = 2 * ay[(i - 1) % n] - my[i - 1];
+    }
+    cout << mx[j % (2 * n)] << " " << my[j % (2 * n)] << "\n";
 }
 
 int main() {
