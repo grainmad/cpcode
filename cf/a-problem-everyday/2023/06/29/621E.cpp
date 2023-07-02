c86c07f25960af95592129e2ac37016c51b2a066
grainmad <2458898755@qq.com>
2023-07-02 09:36:14 +0800

    ape

--- cf/a-problem-everyday/2023/06/29/621E.cpp
+++ cf/a-problem-everyday/2023/06/29/621E.cpp
@@ -1,15 +1,68 @@
 
 #include <bits/stdc++.h>
-// #define SINGLE_INPUT
+#define SINGLE_INPUT
 #define ll long long
 #define ull unsigned long long
-#define N 500005
-#define MOD 998244353
+#define MOD 1000000007
 using namespace std;
 
+#define N 105
+
+struct Matrix {
+    int mat[N][N];
+    int n;
+    Matrix(int n) : n(n) { memset(mat, 0, sizeof(mat)); }
+    inline void operator*=(const Matrix& o) {
+        int ans[n][n] = {};
+        for (int i = 0; i < n; i++)
+            for (int j = 0; j < n; j++)
+                if (mat[i][j])
+                    for (int k = 0; k < n; k++)
+                        ans[i][k] =
+                            (ans[i][k] + (long long)(mat[i][j]) * o.mat[j][k]) %
+                            MOD;
+        for (int i = 0; i < n; ++i)
+            for (int j = 0; j < n; ++j)
+                mat[i][j] = ans[i][j];
+    }
+    void print() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cout << mat[i][j] << " ";
+            }
+            cout << "\n";
+        }
+    }
+};
+/*
+    // a *= b^n
+    for (; n; n >>= 1, b *= b)
+        if (n & 1)
+            a *= b;
+    a.print();
+*/
 
 void sol() {
-
+    int n, b, k, x;
+    cin >> n >> b >> k >> x;
+    vector<int> cnt(10);
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        cnt[t]++;
+    }
+    Matrix A(x), B(x);
+    for (int i = 0; i < x; i++) {
+        for (int j = 1; j < 10; j++) {
+            B.mat[i][(i * 10 + j) % x] += cnt[j];
+        }
+    }
+    A.mat[0][0] = 1;
+    for (; b; b >>= 1, B *= B) {
+        if (b & 1)
+            A *= B;
+    }
+    cout << A.mat[0][k] << "\n";
 }
 
 int main() {
--- cf/a-problem-everyday/2023/06/30/1220D.cpp
+++ cf/a-problem-everyday/2023/06/30/1220D.cpp
@@ -1,15 +1,43 @@
 
 #include <bits/stdc++.h>
-// #define SINGLE_INPUT
+#define SINGLE_INPUT
 #define ll long long
 #define ull unsigned long long
 #define N 500005
 #define MOD 998244353
 using namespace std;
 
-
 void sol() {
-
+    ll n;
+    cin >> n;
+    vector<ll> cnt(100), a(n);
+    ll mx = 0;
+    for (ll i = 0; i < n; i++) {
+        cin >> a[i];
+        ll c = 0;
+        for (ll j = a[i]; j % 2 == 0; j >>= 1)
+            c++;
+        mx = max(mx, ++cnt[c]);
+    }
+    cout << n - mx << "\n";
+    int p = 0;
+    for (ll i : a) {
+        ll c = 0;
+        for (ll j = i; j % 2 == 0; j >>= 1)
+            c++;
+        if (cnt[c] == mx) {
+            p = c;
+            break;
+        }
+    }
+    for (ll i : a) {
+        ll c = 0;
+        for (ll j = i; j % 2 == 0; j >>= 1)
+            c++;
+        if (c != p) {
+            cout << i << " ";
+        }
+    }
 }
 
 int main() {
