6622ce104885a65d1fdd2e8013cd8ff27d42edcd
grainmad <2458898755@qq.com>
2023-07-28 22:32:34 +0800

    tmp ape 26

--- cf/a-problem-everyday/2023/07/26/1029D.cpp
+++ cf/a-problem-everyday/2023/07/26/1029D.cpp
@@ -1,15 +1,39 @@
-
 #include <bits/stdc++.h>
-// #define SINGLE_INPUT
+#define SINGLE_INPUT
 #define ll long long
 #define ull unsigned long long
 #define N 500005
 #define MOD 998244353
 using namespace std;
 
-
 void sol() {
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> len(n), a(n);
+    map<int, int> r[11];
 
+    for (int i = 0; i < n; i++) {
+        ll x;
+        cin >> x;
+        a[i] = x;
+        while (x)
+            len[i]++, x /= 10;
+        r[len[i]][a[i] % k]++;
+    }
+    ll ans = 0;
+    for (int i = 0; i < n; i++) {
+        ll x = a[i];
+        for (int j = 1; j < 11; j++) {
+            x *= 10;
+            x %= k;
+            ll t = (k - x) % k;
+            if (r[j].count(t))
+                ans += r[j][t];
+            if (len[i] == j && (x + a[i] % k) % k == 0)
+                ans--;
+        }
+    }
+    cout << ans << "\n";
 }
 
 int main() {
