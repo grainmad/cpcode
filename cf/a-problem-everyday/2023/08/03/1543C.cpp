7481b334cc54474eb05cf80d33069d98440866ac
grainmad <2458898755@qq.com>
2023-08-04 20:03:06 +0800

    ape

--- cf/a-problem-everyday/2023/08/03/1543C.cpp
+++ cf/a-problem-everyday/2023/08/03/1543C.cpp
@@ -7,9 +7,44 @@
 #define MOD 998244353
 using namespace std;
 
+bool iszero(double x) {
+    return fabs(x) < 1e-8;
+}
 
 void sol() {
-
+    double c, m, p, v;
+    cin >> c >> m >> p >> v;
+    auto dfs = [&](auto self, double x, double y, double z, int stp) -> double {
+        double rt = z * stp;
+        if (!iszero(x)) {
+            if (x > v) {
+                if (iszero(y))
+                    rt += x * self(self, x - v, 0, z + v, stp + 1);
+                else
+                    rt += x * self(self, x - v, y + v / 2, z + v / 2, stp + 1);
+            } else {
+                if (iszero(y))
+                    rt += x * self(self, 0, 0, z + x, stp + 1);
+                else
+                    rt += x * self(self, 0, y + x / 2, z + x / 2, stp + 1);
+            }
+        }
+        if (!iszero(y)) {
+            if (y > v) {
+                if (iszero(x))
+                    rt += y * self(self, 0, y - v, z + v, stp + 1);
+                else
+                    rt += y * self(self, x + v / 2, y - v, z + v / 2, stp + 1);
+            } else {
+                if (iszero(x))
+                    rt += y * self(self, 0, 0, z + y, stp + 1);
+                else
+                    rt += y * self(self, x + y / 2, 0, z + y / 2, stp + 1);
+            }
+        }
+        return rt;
+    };
+    cout << dfs(dfs, c, m, p, 1) << "\n";
 }
 
 int main() {
