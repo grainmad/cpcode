b2b722d28e489bd9d0ea0bae8197ada77e783aba
grainmad <2458898755@qq.com>
2023-08-30 17:15:30 +0800

    tmp & ape 8.30

--- cf/a-problem-everyday/2023/08/12/1784B.cpp
+++ cf/a-problem-everyday/2023/08/12/1784B.cpp
@@ -7,38 +7,76 @@
 #define MOD 998244353
 using namespace std;
 
-int mp[200];
-string ch = "win";
-
 void sol() {
-    mp['w'] = 0;
-    mp['i'] = 1;
-    mp['n'] = 2;
-    int n;
-    cin >> n;
-    vector<int> cnt[3][3][3];
-    for (int i = 1; i <= n; i++) {
+    int m;
+    cin >> m;
+    map<string, vector<int>> mp;
+    for (int i = 1; i <= m; i++) {
         string s;
         cin >> s;
-        int c[3] = {0};
-        for (int i : s)
-            c[mp[i]]++;
-        cnt[c[0]][c[1]][c[2]].push_back(i);
+        sort(s.begin(), s.end());
+        // cout << s << endl;
+        mp[s].push_back(i);
     }
-    int ans = 0;
-    for (int i = 0; i < 3; i++) {
-        int a[3] = {0}, b[3] = {0};
-        a[i] = 3;
-        b[(i + 1) % 3] = 3;
-        while (cnt[a[0]][a[1]][a[2]].size() && cnt[b[0]][b[1]][b[2]].size()) {
-            int x = cnt[a[0]][a[1]][a[2]].back(),
-                y = cnt[b[0]][b[1]][b[2]].back();
-            cout << x << " " << ch[i] << " " << y << " " << ch[(i + 1) % 3]
-                 << "\n";
-            a[i]--;
-            b[(i + 1) % 3]++;
-            cnt[a[0]][a[1]][a[2]];
+    vector<string> ans;
+    // 三个全相同
+    while (mp["www"].size() && mp["iii"].size() && mp["nnn"].size()) {
+        ans.emplace_back(to_string(mp["www"].back()) + " w " +
+                         to_string(mp["iii"].back()) + " i");
+        ans.emplace_back(to_string(mp["www"].back()) + " w " +
+                         to_string(mp["nnn"].back()) + " n");
+        ans.emplace_back(to_string(mp["iii"].back()) + " i " +
+                         to_string(mp["nnn"].back()) + " n");
+        mp["www"].pop_back();
+        mp["iii"].pop_back();
+        mp["nnn"].pop_back();
+    }
+    // 含一个全相同 两次交换恢复3个
+    auto case1 = [&](const string& a, const string& b, const string& c) {
+        while (mp[a].size() && mp[b].size() && mp[c].size()) {
+            ans.emplace_back(to_string(mp[a].back()) + " " + a[1] + " " +
+                             to_string(mp[b].back()) + " " + b[1]);
+            ans.emplace_back(to_string(mp[a].back()) + " " + a[1] + " " +
+                             to_string(mp[c].back()) + " " + c[1]);
+            mp[a].pop_back();
+            mp[b].pop_back();
+            mp[c].pop_back();
         }
+    };
+    case1("www", "iin", "inn");
+    case1("iii", "nww", "nnw");
+    case1("nnn", "iiw", "iww");
+    // 不含全相同 两次交换恢复3个
+    auto case2 = [&](const string& a, const string& b, const string& c) {
+        while (mp[a].size() && mp[b].size() && mp[c].size()) {
+            ans.emplace_back(to_string(mp[a].back()) + " w " +
+                             to_string(mp[b].back()) + " " + b[1]);
+            ans.emplace_back(to_string(mp[b].back()) + " w " +
+                             to_string(mp[c].back()) + " " + c[1]);
+            mp[a].pop_back();
+            mp[b].pop_back();
+            mp[c].pop_back();
+        }
+    };
+    case2("nww", "iiw", "inn");
+    case2("iww", "nnw", "iin");
+
+    // 一次交换恢复两个
+    auto case3 = [&](const string& a, const string& b) {
+        while (mp[a].size() && mp[b].size()) {
+            ans.emplace_back(to_string(mp[a].back()) + " " + a[1] + " " +
+                             to_string(mp[b].back()) + " " + b[1]);
+            mp[a].pop_back();
+            mp[b].pop_back();
+        }
+    };
+    case3("iww", "inn");
+    case3("nww", "iin");
+    case3("iiw", "nnw");
+
+    cout << ans.size() << "\n";
+    for (auto i : ans) {
+        cout << i << "\n";
     }
 }
 
---
+++ cf/a-problem-everyday/2023/08/30/1716D.cpp
@@ -0,0 +1,144 @@
+
+#include <bits/stdc++.h>
+#define SINGLE_INPUT
+#define ll long long
+#define ull unsigned long long
+#define N 500005
+#define MOD 998244353
+using namespace std;
+
+// #define MOD 1000000007
+#define mint Modint<MOD>
+template <const int _MOD>
+struct Modint {
+    int v;
+    Modint() { v = 0; }
+    Modint(long long o) { v = o % _MOD; }
+    int val() { return v; }
+    int pow(long long o) {
+        int ret = 1, tmp = v;
+        while (o) {
+            if (o & 1)
+                ret = ((long long)ret * tmp) % _MOD;
+            o >>= 1;
+            tmp = ((long long)tmp * tmp) % _MOD;
+        }
+        return ret;
+    }
+    void operator=(long long o) { v = o % _MOD; }
+    bool operator==(long long o) const { return v == o; }
+    bool operator==(Modint o) const { return v == o.v; }
+    bool operator!=(long long o) const { return v != o; }
+    bool operator!=(Modint o) const { return v != o.v; }
+    bool operator<(long long o) const { return v < o; }
+    bool operator<(Modint o) const { return v < o.v; }
+    bool operator>(long long o) const { return v > o; }
+    bool operator>(Modint o) const { return v > o.v; }
+    bool operator<=(long long o) const { return v <= o; }
+    bool operator<=(Modint o) const { return v <= o.v; }
+    bool operator>=(long long o) const { return v >= o; }
+    bool operator>=(Modint o) const { return v >= o.v; }
+
+    Modint operator+(long long o) const { return *this + Modint(o); }
+    Modint operator+(Modint o) const { return ((long long)v + o.v) % _MOD; }
+    Modint operator*(long long o) const { return *this * Modint(o); }
+    Modint operator*(Modint o) const { return (long long)v * o.v % _MOD; }
+
+    Modint operator-(long long o) const { return *this - Modint(o); }
+    Modint operator-(Modint o) const {
+        return ((long long)v - o.v + _MOD) % _MOD;
+    }
+
+    Modint operator/(long long o) const { return *this / Modint(o); }
+    Modint operator/(Modint o) const {
+        return ((long long)v * o.pow(_MOD - 2)) % _MOD;
+    }
+    void operator+=(long long o) { *this = *this + o; }
+    void operator+=(Modint o) { *this = *this + o; }
+    void operator*=(long long o) { *this = *this * o; }
+    void operator*=(Modint o) { *this = *this * o; }
+    void operator-=(long long o) { *this = *this - o; }
+    void operator-=(Modint o) { *this = *this - o; }
+    void operator/=(long long o) { *this = *this / o; }
+    void operator/=(Modint o) { *this = *this / o; }
+
+    Modint operator^(long long o) { return Modint(pow(o)); }
+    Modint operator^(Modint o) { return Modint(pow(o.v)); }
+
+    template <class T>
+    friend bool operator==(T o, Modint u) {
+        return u == o;
+    }
+    template <class T>
+    friend Modint operator+(T o, Modint u) {
+        return u + o;
+    }
+    template <class T>
+    friend Modint operator*(T o, Modint u) {
+        return u * o;
+    }
+    template <class T>
+    friend Modint operator-(T o, Modint u) {
+        return Modint(o) - u;
+    }
+    template <class T>
+    friend Modint operator/(T o, Modint u) {
+        return Modint(o) / u;
+    }
+
+    void operator++() { *this = *this + 1; }
+    void operator--() { *this = *this - 1; }
+    void operator++(int k) { *this = *this + 1; }
+    void operator--(int k) { *this = *this - 1; }
+
+    template <const int T>
+    friend std::istream& operator>>(std::istream& in, Modint<T>& modint) {
+        ll x;
+        in >> x;
+        modint = Modint<T>(x);
+        return in;
+    }
+
+    template <const int T>
+    friend std::ostream& operator<<(std::ostream& os, const Modint<T>& modint) {
+        os << modint.v;
+        return os;
+    }
+};
+
+void sol() {
+    int k, n;
+    cin >> n >> k;
+    vector<mint> f(n + 1), ans(n + 1);
+    f[0] = 1;
+    for (int c = 0; c + k <= n; c += k++) {  // iterate sqrt n
+        vector<mint> s(k);
+        for (int i = c; i <= n; i++) {
+            mint t = f[i];
+            f[i] = s[i % k];
+            s[i % k] += t;
+            ans[i] += f[i];
+        }
+    }
+    for (int i = 1; i <= n; i++) {
+        cout << ans[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    cout << setprecision(15) << fixed;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef SINGLE_INPUT
+    int t;
+    cin >> t;
+    while (t--) {
+        sol();
+    }
+#else
+    sol();
+#endif
+    return 0;
+}
