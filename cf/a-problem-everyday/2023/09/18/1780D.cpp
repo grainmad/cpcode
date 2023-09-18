7bce61aea055775eb4dffa53ea3bf24a85766a89
aliyun <aliyun@xxx.com>
2023-09-18 18:01:48 +0800

    ape 9.18

--- cf/a-problem-everyday/2023/09/18/1780D.cpp
+++ cf/a-problem-everyday/2023/09/18/1780D.cpp
@@ -7,9 +7,42 @@
 #define MOD 998244353
 using namespace std;
 
+ll cur = 231232195;
+
+ll cntbit(ll x) {
+    ll cnt = 0;
+    while (x) {
+        cnt += x&1;
+        x>>=1;
+    }
+    return cnt;
+}
+
+ll getn(ll x) {
+    cur -= x;
+    cout << cur << endl;
+    return cntbit(cur);
+}
+
 
 void sol() {
-
+    ll n;
+    // n = getn(0);
+    cin >> n;
+    ll b = 0, p = n, d = 0;
+    ll ans = 0;
+    while (d < n) {
+        cout << "- " << (1LL<<b+1) - (1LL<<b-d) << endl;
+        p = n;
+        // n = getn((1LL<<b+1) - (1LL<<b-d));
+        cin >> n;
+        if (n == -1) return ;
+        d = n-(p-d)+1;
+        b += d;
+        // cout << "d: " << d << " p: " << p << " n: " << n << " b: " << b << endl;
+        ans |= 1<<b;
+    }
+    cout << "! " << ans << endl;
 }
 
 int main() {
