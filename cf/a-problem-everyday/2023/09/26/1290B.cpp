2711ddad0c2f73aba60f13b44537aaaa79c80a9e
grainmad <2458898755@qq.com>
2023-10-11 21:15:36 +0800

    ape

--- cf/a-problem-everyday/2023/09/26/1290B.cpp
+++ cf/a-problem-everyday/2023/09/26/1290B.cpp
@@ -1,15 +1,40 @@
 
 #include <bits/stdc++.h>
-// #define SINGLE_INPUT
+#define SINGLE_INPUT
 #define ll long long
 #define ull unsigned long long
 #define N 500005
 #define MOD 998244353
 using namespace std;
 
-
 void sol() {
-
+    string s;
+    cin >> s;
+    int n = s.size();
+    vector c(n + 1, vector<int>(26, 0));
+    for (int i = 1; i <= n; i++) {
+        for (int j = 0; j < 26; j++)
+            c[i][j] = c[i - 1][j];
+        c[i][s[i - 1] - 'a']++;
+    }
+    auto check = [&](int l, int r) {
+        int d = 0;
+        for (int i = 0; i < 26; i++) {
+            d += c[r][i] != c[l - 1][i];
+        }
+        return d >= 3;
+    };
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; i++) {
+        int l, r;
+        cin >> l >> r;
+        if (l == r || s[l - 1] != s[r - 1] || check(l, r)) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
 }
 
 int main() {
