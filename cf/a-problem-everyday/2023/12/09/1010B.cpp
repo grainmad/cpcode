ea6a4c5a311e043fc673b97b5c7f1bccef4c1318
grainmad <2458898755@qq.com>
2023-12-09 19:22:50 +0800

    ape

--- cf/a-problem-everyday/2023/12/09/1010B.cpp
+++ cf/a-problem-everyday/2023/12/09/1010B.cpp
@@ -1,6 +1,5 @@
-
 #include <bits/stdc++.h>
-// #define SINGLE_INPUT
+#define SINGLE_INPUT
 #define ll long long
 #define ull unsigned long long
 #define N 500005
@@ -11,18 +10,21 @@ vector<int> p = {1, 0, 1, 0, 1, 0};
 int x = 10;
 
 int ask(int y, int t) {
+    cout << y << endl;
     int rt = 0;
-    if (y > x)
-        rt = -1;
-    if (y < x)
-        rt = 1;
-    return p[t % p.size()] ? rt : -rt;
+    cin >> rt;
+    return rt;
+    // if (y > x)
+    //     rt = -1;
+    // if (y < x)
+    //     rt = 1;
+    // return p[t % p.size()] ? rt : -rt;
 }
 
 void sol() {
     int m, n;
-    // cin >> m >> n;
-    m = 50, n = 6;
+    cin >> m >> n;
+    // m = 50, n = 6;
     vector<int> c(n);
     for (int i = 0; i < n; i++) {
         int rt = ask(1, i);
@@ -34,8 +36,8 @@ void sol() {
     }
     int l = 1, r = m + 1;
     for (int i = n; i < 60; i++) {
-        int m = l + r >> 1;
-        int rt = ask(m, i);
+        int mid = l + r >> 1;
+        int rt = ask(mid, i);
         if (rt == 0) {
             return;
         }
@@ -43,9 +45,9 @@ void sol() {
             rt = -rt;
         }
         if (rt == 1) {
-            l = m + 1;
+            l = mid + 1;
         } else {
-            r = m;
+            r = mid;
         }
     }
 }
