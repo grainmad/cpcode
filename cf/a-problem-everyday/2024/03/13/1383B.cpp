1d1e14d8ac5b336c69d6f75f84f457cf3dc1b652
grainmad <2458898755@qq.com>
2024-03-13 19:48:26 +0800

    ape:思维博弈

---
+++ cf/a-problem-everyday/2024/03/13/1383B.cpp
@@ -0,0 +1,62 @@
+
+#include <bits/stdc++.h>
+// #define SINGLE_INPUT
+#define ll long long
+#define ull unsigned long long
+#define N 500005
+#define MOD 998244353
+using namespace std;
+
+template<class t,class u> ostream& operator<<(ostream& os,const pair<t,u>& p) {
+    return os<<'['<<p.first<<", "<<p.second<<']';
+}
+template<class t> ostream& operator<<(ostream& os,const vector<t>& v) {
+    os<<'['; int s = 1;
+    for(auto e:v) { if (s) s = 0; else os << ", "; os << e; }
+    return os<<']';
+}
+template<class t,class u> ostream& operator<<(ostream& os,const map<t,u>& mp){
+    os<<'{'; int s = 1;
+    for(auto [x,y]:mp) { if (s) s = 0; else os << ", "; os<<x<<": "<<y; }
+    return os<<'}';
+}
+
+void sol() {
+    int n;
+    cin >> n;
+    vector<int> a(n);
+    for (auto& i:a) cin >> i;
+    for (int i=31; i>=0; i--) {
+        int c1 = 0, c0 = 0;
+        for (int j:a) {
+            if (j>>i&1) c1++;
+            else c0++;
+        }
+        if (c1%2) {
+            if (c1/2%2) {
+                cout << (c0%2?"WIN\n":"LOSE\n");
+            } else {
+                cout << "WIN\n";
+            }
+            return ;
+        }
+    }
+    cout << "DRAW\n";
+}
+
+int main() {
+    cout << setprecision(15) << fixed;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+#ifndef SINGLE_INPUT
+    int t;
+    cin >> t;
+    while (t--) {
+        sol();
+    }
+#else
+    sol();
+#endif
+    return 0;
+}
